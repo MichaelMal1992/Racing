23948bb5d0c366e9f4473c64295b2969bd92d183
MichaelMal1992 <>
2021-08-15 18:39:50 +0300

    Structured project

--- RacingCarGame/RacingCarGame/SceneDelegate/SceneDelegate.swift
+++ RacingCarGame/RacingCarGame/SceneDelegate/SceneDelegate.swift
@@ -14,23 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else {
+        guard (scene as? UIWindowScene) != nil else {
             return
         }
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
     }
 }
--- RacingCarGame/RacingCarGame/ViewControllers/StartGame/StartGameViewController.swift
+++ RacingCarGame/RacingCarGame/ViewControllers/StartGame/StartGameViewController.swift
@@ -668,25 +668,3 @@ class StartGameViewController: UIViewController {
         exit(0)
     }
 }
-
-extension StartGameViewController: AVAudioPlayerDelegate {
-
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        switch player {
-        case Sound.game:
-            if flag {
-                Sound.game.play()
-            }
-        case Sound.move:
-            if flag {
-                Sound.move.play()
-            }
-        case Sound.trafic:
-            if flag {
-                Sound.trafic.play()
-            }
-        default:
-            return
-        }
-    }
-}
