3eff69ea3c1b62b5c026286decf91b9d85c8446f
MichaelMal1992 <>
2021-08-29 23:11:55 +0300

    Add files

---
+++ RacingCarGame/RacingCarGame/Managers/RealmManager.swift
@@ -0,0 +1,56 @@
+//
+//  RealmManager.swift
+//  RacingCarGame
+//
+//  Created by Admin on 23.08.21.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+
+    static let shared = RealmManager()
+
+    private let realm: Realm?
+
+    private init() {
+        do {
+            self.realm = try Realm()
+        } catch {
+            print(error.localizedDescription)
+            fatalError()
+        }
+    }
+
+    func add(_ object: PlayerData) {
+        do {
+            try realm?.write {
+                realm?.add(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    var allPlayers: [PlayerData] {
+        if let player = realm?.objects(PlayerData.self) {
+            return Array(player)
+        }
+        return []
+    }
+
+    func write(_ completion: () -> Void) {
+        do {
+            try realm?.write {
+            completion()
+           }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func deleteAll() {
+        realm?.deleteAll()
+    }
+}
